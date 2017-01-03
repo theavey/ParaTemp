96cd35d2e65123329db4076c61efbe75492dd411
Thomas Heavey <thomasjheavey@gmail.com>
2017-01-03 15:39:22 -0500

    Added functions to find and plot important oxygen distances

--- CoordinateAnalysis.py
+++ CoordinateAnalysis.py
@@ -96,8 +96,7 @@ def get_dihedral(a, b, c, d, units='rad'):
     bc = b.centroid() - c.centroid()
     dc = d.centroid() - c.centroid()
     angle = arctan2(dot(cross(cross(ba, bc), cross(bc, dc)), bc) /
-                    norm(bc),
-                    dot(cross(ba, bc), cross(bc, dc)))
+                    norm(bc), dot(cross(ba, bc), cross(bc, dc)))
     if units == 'rad':
         return angle
     elif units == 'deg':
@@ -113,3 +112,37 @@ def get_dihedral_dict(dictionary, a, b, c, d, units='rad'):
     return get_dihedral(dictionary[a], dictionary[b],
                         dictionary[c], dictionary[d],
                         units=units)
+
+
+def get_taddol_ox_dists(universe, sel_dict=False):
+    """Get array of oxygen distances in TADDOL trajectory"""
+    from numpy import array
+    if not sel_dict:
+        sel_dict = get_taddol_selections(universe)
+    output = []
+    for frame in universe.trajectory:
+        output.append((universe.trajectory.time,
+                       get_dist_dict(sel_dict, 'aoxl', 'aoxr'),
+                       get_dist_dict(sel_dict, 'aoxl', 'cyclon'),
+                       get_dist_dict(sel_dict, 'aoxr', 'cyclon')))
+    return array(output)
+
+
+def make_plot_taddol_ox_dists(data, save=False, save_format='pdf',
+                              save_base_name='ox_dists',
+                              display=True):
+    """"""
+    from matplotlib.pyplot import subplots
+    fig, axes = subplots()
+    axes.plot(data[:, 0], data[:, 1], label='O-O')
+    axes.plot(data[:, 0], data[:, 2], label='O(l)-Cy')
+    axes.plot(data[:, 0], data[:, 3], label='O(r)-Cy')
+    axes.legend()
+    axes.set_xlabel('time / ps')
+    axes.set_ylabel('distance / $\mathrm{\AA}$')
+    if save:
+        fig.savefig(save_base_name+save_format)
+    if display:
+        return fig
+    else:
+        return None
