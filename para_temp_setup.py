b149f3b449b9f47e88ab0e9349d8f36d935923c0
Thomas Heavey <thomasjheavey@gmail.com>
2017-01-24 14:51:07 -0500

    Changed para_temp_setup to function (and function call if used on the command line (should not change API)

--- para_temp_setup.py
+++ para_temp_setup.py
@@ -26,10 +26,66 @@
 # be working on the newest version of python.
 
 
+def compile_tprs(template='templatemdp.txt', start_temp=205., number=16,
+                 scaling_exponent=0.025, base_name='npt',
+                 topology='../*top', multi_structure=False,
+                 structure='../*gro', index='../index.ndx',
+                 temps_file='temperatures.dat'):
+    """
+    Compile TPR files for REMD run with GROMACS
+
+    :param template:
+    :param start_temp:
+    :param number:
+    :param scaling_exponent:
+    :param base_name:
+    :param topology:
+    :param multi_structure:
+    :param structure:
+    :param index:
+    :param temps_file:
+    :return:
+    """
+    # if args.multi_structure:
+    from glob import glob
+    structures = glob(structure+'*.gro')
+    structures.sort()
+    structures.sort(key=len)
+    temps = []
+    from math import exp
+    for i in range(number):
+        mdp_name = base_name + str(i) + '.mdp'
+        temp = start_temp * exp(i * scaling_exponent)
+        temps += [temp]
+        if multi_structure:
+            structure = structures[i]
+        with open(template, 'r') as f_template, \
+                open(mdp_name, 'w') as out_file:
+            for line in f_template:
+                if 'TempGoesHere' in line:
+                    line = line.replace('TempGoesHere', str(temp))
+                out_file.write(line)
+        command_line = ['grompp_mpi',
+                        '-f', mdp_name,
+                        '-p', topology,
+                        '-c', structure,
+                        '-n', index,
+                        '-o', mdp_name.replace('mdp', 'tpr'),
+                        '-maxwarn', '2']
+        with open('gromacs_compile_output.log', 'w') as log_file:
+            from subprocess import Popen, PIPE
+            proc = Popen(command_line,
+                         stdout=PIPE, bufsize=1,
+                         universal_newlines=True)
+            for line in proc.stdout:
+                log_file.write(line)
+    with open(temps_file, 'w') as temps_out:
+        temps_out.write(str(temps))
+        temps_out.write('\n')
+
+
 if __name__ == "__main__":
     import argparse
-    import math
-    import subprocess
 
     __version__ = '0.1.3'
 
@@ -49,7 +105,6 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--topology',
                         default='../taddol_3htmf_stilbene_em.top',
                         help='name of topology file (.top)')
-    # todo accept different structure files (a series/list of them)
     parser.add_argument('-m', '--multi_structure', default=False,
                         help='Use multiple starting structure files')
     parser.add_argument('-c', '--structure', default='../major_endo.gro',
@@ -62,44 +117,16 @@ if __name__ == "__main__":
                         version='%(prog)s v{}'.format(__version__))
     args = parser.parse_args()
 
-    # todo see if argparse can do any type checking
-    number = int(args.number)
-    start_temp = float(args.start_temp)
-    scaling_exponent = float(args.scaling_exponent)
-
-    # if args.multi_structure:
-    from glob import glob
-    structures = glob(args.structure+'*.gro')
-    structures.sort()
-    structures.sort(key=len)
-    # todo make this a function and call it
-    temps = []
-    for i in range(number):
-        mdp_name = args.base_name + str(i) + '.mdp'
-        temp = start_temp * math.exp(i * scaling_exponent)
-        temps += [temp]
-        if args.multi_structure:
-            structure = structures[i]
-        else:
-            structure = args.structure
-        with open(args.template, 'r') as template, \
-                open(mdp_name, 'w') as out_file:
-            for line in template:
-                if 'TempGoesHere' in line:
-                    line = line.replace('TempGoesHere', str(temp))
-                out_file.write(line)
-        command_line = ['grompp_mpi',
-                        '-f', mdp_name,
-                        '-p', args.topology,
-                        '-c', structure,
-                        '-n', args.index,
-                        '-o', mdp_name.replace('mdp', 'tpr'),
-                        '-maxwarn', '2']
-        with open('gromacs_compile_output.log', 'w') as log_file:
-            proc = subprocess.Popen(command_line,
-                                    stdout=subprocess.PIPE, bufsize=1,
-                                    universal_newlines=True)
-            for line in proc.stdout:
-                log_file.write(line)
-    with open(args.temps_file, 'w') as temps_out:
-        temps_out.write(str(temps))
+    # TODO see if argparse can do any type checking
+    compile_tprs(
+        template=args.template,
+        start_temp=float(args.start_temp),
+        number=int(args.number),
+        scaling_exponent=float(args.scaling_exponent),
+        base_name=args.base_name,
+        topology=args.topology,
+        multi_structure=args.multi_structure,
+        structure=args.structure,
+        index=args.index,
+        temps_file=args.temps_file
+    )
