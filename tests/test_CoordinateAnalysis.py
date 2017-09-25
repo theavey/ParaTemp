74ec4a817fa1e9b660946988a0690eccd761c329
Thomas Heavey <thomasjheavey@gmail.com>
2017-09-25 11:51:13 -0400

    Added tests folder and first test

---
+++ tests/test_CoordinateAnalysis.py
@@ -0,0 +1,31 @@
+
+"""This contains a set of tests for ParaTemp.CoordinateAnalysis"""
+
+########################################################################
+#                                                                      #
+# This script was written by Thomas Heavey in 2017.                    #
+#        theavey@bu.edu     thomasjheavey@gmail.com                    #
+#                                                                      #
+# Copyright 2017 Thomas J. Heavey IV                                   #
+#                                                                      #
+# Licensed under the Apache License, Version 2.0 (the "License");      #
+# you may not use this file except in compliance with the License.     #
+# You may obtain a copy of the License at                              #
+#                                                                      #
+#    http://www.apache.org/licenses/LICENSE-2.0                        #
+#                                                                      #
+# Unless required by applicable law or agreed to in writing, software  #
+# distributed under the License is distributed on an "AS IS" BASIS,    #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      #
+# implied.                                                             #
+# See the License for the specific language governing permissions and  #
+# limitations under the License.                                       #
+#                                                                      #
+########################################################################
+
+from ParaTemp import CoordinateAnalysis as ca
+
+
+def test_running_mean():
+    tl = [0, 2, 4]
+    assert ca.Taddol._running_mean(tl) == [1, 3]
