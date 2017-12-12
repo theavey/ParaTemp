b471bfa9a60e5e01a70044cf6481716db38e1e6f
Thomas Heavey <thomasjheavey@gmail.com>
2017-12-12 14:40:47 -0500

    add setup.(py|cfg), prep for versioneer versioning

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ParaTemp',
+    version='0.0.2-alpha',
+    packages=['paratemp'],
+    url='https://github.com/theavey/ParaTemp',
+    license='Apache License 2.0',
+    author='Thomas Heavey',
+    author_email='thomasjheavey@gmail.com',
+    description='Scripts for molecular dynamics analysis and parallel tempering in GROMACS'
+)
