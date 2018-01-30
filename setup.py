3fbb0801667840042c05cb2e1fbfe36c48b57beb
Thomas Heavey <thomasjheavey@gmail.com>
2018-01-30 16:52:27 -0500

    ParaTemp-#4 Make travis-ci use locally installed version
    Update some dates; add check-convergence script to setup.py; add
    appropriate line break for long string

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 import versioneer
 
 setup(
@@ -6,9 +6,11 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     packages=['paratemp'],
+    scripts=['check-convergence.py'],
     url='https://github.com/theavey/ParaTemp',
     license='Apache License 2.0',
     author='Thomas Heavey',
     author_email='thomasjheavey@gmail.com',
-    description='Scripts for molecular dynamics analysis and parallel tempering in GROMACS'
+    description='Scripts for molecular dynamics analysis and parallel '
+                'tempering in GROMACS'
 )
