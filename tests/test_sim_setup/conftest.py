c79c39c61b542c2a56dbabadcedbe06245387e8e
Thomas Heavey <thomasjheavey@gmail.com>
2019-10-11 12:04:24 -0400

    use mol2 to not require openbabel

--- tests/test_sim_setup/conftest.py
+++ tests/test_sim_setup/conftest.py
@@ -10,7 +10,7 @@ import pytest
 def molecule(path_test_data, tmp_path):
     from paratemp import cd
     from paratemp.sim_setup import Molecule
-    path_gro = path_test_data / 'water.pdb'
+    path_gro = path_test_data / 'water.mol2'
     # Note: this instantiation will make a new directory!
     with cd(tmp_path):
         mol = Molecule(path_gro)
